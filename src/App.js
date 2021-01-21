d717f9e41db001fb7758d75d45e1c7fff19731b3
rabeb wederni <rabebfarah2015@gmail.com>
2021-01-22 01:28:24 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './Style/style.css'
+import './App.css'
+import pic from './images/img1.jpg'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundImage: `url(https://image.freepik.com/free-vector/stone-background-with-golden-strokes_125540-130.jpg)`}} >
+    <div >
+      <h1 className="title red">PROJECT checkpoint-jsx</h1>
+      <br /><br />
+      <i><img className="img" src={pic}  alt="img1"/></i>
+      <i><img className="img" src="/img2.jpg" alt="img2"/></i>
     </div>
+    <br /><br />
+    <video className="video" width={320} height={240} controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+  </div>
   );
 }
+
+
+
+
 
 export default App;
